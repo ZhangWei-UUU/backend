8ef616ad2bbc1affd7718dfc7c6e34d25682bddd
ZhangWei <350095093@qq.com>
2019-01-13 15:12:43 +0800

    add test2

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => res.send({name:"zhangwei"}))
+app.get("/test2", (req, res) => res.send({name:"zhangwei2"}))
 
 app.listen(3010, () => console.log('Example app listening on port 3010!'))
