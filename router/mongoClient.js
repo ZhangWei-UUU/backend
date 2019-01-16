58c96b20297da92249e426f4db57cafda28beafd
ZhangWei <350095093@qq.com>
2019-01-16 17:55:47 +0800

    add crud functions

--- router/mongoClient.js
+++ router/mongoClient.js
@@ -13,6 +13,45 @@ module.exports = {
       }finally {
         client.close();
       }
+    },
+
+    insertSingle: async (object,collectionName) => {
+        let db, client;
+        try {
+          client = await MongoClient.connect(DB_CONFIG.url, { useNewUrlParser: true });
+          db = client.db(DB_CONFIG.dbname);
+          return await db.collection(collectionName).insertOne(object);
+        } catch(err){
+          throw new Error(err)
+        }finally {
+          client.close();
+        }
+    },
+
+    updateSingle: async (updateObj,userId,collectionName) => {
+        let db, client;
+        try {
+          client = await MongoClient.connect(DB_CONFIG.url, { useNewUrlParser: true });
+          db = client.db(DB_CONFIG.dbname);
+          return await db.collection(collectionName).updateOne({object})
+        } catch(err){
+          throw new Error(err)
+        }finally {
+          client.close();
+        }
+    },
+
+    deleteSingle: async (id,userId,collectionName) => {
+        let db, client;
+        try {
+          client = await MongoClient.connect(DB_CONFIG.url, { useNewUrlParser: true });
+          db = client.db(DB_CONFIG.dbname);
+          return await db.collection(collectionName).remove({id:id})
+        } catch(err){
+          throw new Error(err)
+        }finally {
+          client.close();
+        }
     }
   }
   
--- router/order/index.js
+++ router/order/index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const {queryData,insertSingle,deleteSingle,updateSingle} = require('../mongoClient');
 
-router.get('/', function(req, res) {
-  res.send({title:"/ws"});
+router.get('/orders', function(req, res) {
+    const result = await queryData({},"orders");
+    res.send(result)
+});
+
+
+router.post('/insertSingleOrder', function(req, res) {
+    const result = await insertSingle(object,"orders");
+    res.send(result)
+});
+
+router.delete('/deleteSingleOrder/:id', function(req, res) {
+    const result = await deleteSingle(req.params.id,"orders");
+    res.send(result)
+});
+
+router.post('/updateSingleOrder', function(req, res) {
+    const result = await updateSingle(object,"orders");
+    res.send(result)
 });
 
 module.exports = router;
